182910656fd7816605493b85dcf96d5ad396baf6
Vanshika Srivastava <vanshikasrivastava.business@gmail.com>
2024-08-29 22:07:36 +0400

    update app.jsx wto use CirclesSDKProvider as react context

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,28 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './dashboard';
 import CirclesOnboarding from './circlesonboarding';
-
+import { CirclesSDKProvider } from './contexts/CirclesSDK';
 
 export default function App() {
   const [trustRelations, setTrustRelations] = useState([]);
 
-
-
   return (
-    <Router>
-      <Routes>
-        {/* Route for Circles Onboarding */}
-        <Route
-          path="/"
-          element={<CirclesOnboarding setTrustRelations={setTrustRelations}/>}
-        />
+    <CirclesSDKProvider>  
+      <Router>
+        <Routes>
+          {/* Route for Circles Onboarding */}
+          <Route
+            path="/"
+            element={<CirclesOnboarding setTrustRelations={setTrustRelations} />}
+          />
 
-        {/* Route for Dashboard */}
-        <Route
-          path="/dashboard"
-          element={<Dashboard trustRelations={trustRelations} />}
-        />
-      </Routes>
-    </Router>
-  
+          {/* Route for Dashboard */}
+          <Route
+            path="/dashboard"
+            element={<Dashboard trustRelations={trustRelations} />}
+          />
+        </Routes>
+      </Router>
+    </CirclesSDKProvider>
   );
 }
