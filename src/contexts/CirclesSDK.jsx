cb73cd2f7a2b1aff20e7b41727cedb4f8b5d9d83
Vanshika Srivastava <vanshikasrivastava.business@gmail.com>
2024-09-15 00:04:05 +0400

    update chain config

--- src/contexts/CirclesSDK.jsx
+++ src/contexts/CirclesSDK.jsx
@@ -15,10 +15,10 @@ export const CirclesSDK = ({ children }) => {
         pathfinderUrl: 'https://pathfinder.aboutcircles.com',
         circlesRpcUrl: 'https://rpc.helsinki.aboutcircles.com',
         v1HubAddress: '0x29b9a7fbb8995b2423a71cc17cf9810798f6c543',
-        v2HubAddress: '0x7bC1F123089Bc1f384b6379d0587968d1CD5830a',
-        migrationAddress: '0xEaBa6046103C3A2f5A681fD4323f78C647Fb4292',
+        v2HubAddress: '0xa5c7ADAE2fd3844f12D52266Cb7926f8649869Da',
+        migrationAddress: '0xe1dCE89512bE1AeDf94faAb7115A1Ba6AEff4201',
+        nameRegistryAddress: '0x738fFee24770d0DE1f912adf2B48b0194780E9AD',
         profileServiceUrl: 'https://chiado-pathfinder.aboutcircles.com/profiles/',
-        nameRegistryAddress: "0xb95ef3f3e693531d9588815bca954dc8dce30937",
     };
 
     const initSdk = useCallback(async () => {
