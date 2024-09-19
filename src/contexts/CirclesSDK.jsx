4921ac965893bb07953c180d105d53eb0ba5e483
Vanshika Srivastava <vanshikasrivastava.business@gmail.com>
2024-09-20 03:13:35 +0800

    upgrade to 0.9.0 and circlesRPCURL

--- src/contexts/CirclesSDK.jsx
+++ src/contexts/CirclesSDK.jsx
@@ -13,7 +13,7 @@ export const CirclesSDK = ({ children }) => {
 
   const chainConfig = {
     pathfinderUrl: "https://pathfinder.aboutcircles.com",
-    circlesRpcUrl: "https://rpc.helsinki.aboutcircles.com",
+    circlesRpcUrl: "https://rpc.falkenstein.aboutcircles.com",
     v1HubAddress: "0x29b9a7fbb8995b2423a71cc17cf9810798f6c543",
     v2HubAddress: "0xa5c7ADAE2fd3844f12D52266Cb7926f8649869Da",
     migrationAddress: "0xe1dCE89512bE1AeDf94faAb7115A1Ba6AEff4201",
