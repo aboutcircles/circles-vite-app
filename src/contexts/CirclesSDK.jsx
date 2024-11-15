35cd1eea30d6a7c7094603a78d1388ab6bd14dc6
Vanshika Srivastava <vanshikasrivastava.business@gmail.com>
2024-11-15 17:35:18 +0530

    Update CirclesSDK.jsx change rpc URLs

--- src/contexts/CirclesSDK.jsx
+++ src/contexts/CirclesSDK.jsx
@@ -14,14 +14,12 @@ export const CirclesSDK = ({ children }) => {
   const [circlesData, setCirclesData] = useState(null);
 
   const chainConfig = {
-    pathfinderUrl: "https://pathfinder.aboutcircles.com",
-    circlesRpcUrl: "https://rpc.falkenstein.aboutcircles.com",
-    // circlesRpcUrl: "https://rpc.aboutcircles.com/",
+    circlesRpcUrl: "https://static.94.138.251.148.clients.your-server.de/rpc/",
     v1HubAddress: "0x29b9a7fbb8995b2423a71cc17cf9810798f6c543",
     v2HubAddress: "0x3D61f0A272eC69d65F5CFF097212079aaFDe8267",
     migrationAddress: "0x28141b6743c8569Ad8B20Ac09046Ba26F9Fb1c90",
     nameRegistryAddress: "0x8D1BEBbf5b8DFCef0F7E2039e4106A76Cb66f968",
-    profileServiceUrl: "https://rpc.aboutcircles.com/profiles/",
+    profileServiceUrl: "https://static.94.138.251.148.clients.your-server.de/profiles/",
     baseGroupMintPolicy: "0x79Cbc9C7077dF161b92a745345A6Ade3fC626A60",
   };
 
